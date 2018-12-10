9b570b9136beb2cd2c6216f89cd96f7242bf5339
dudulins <dudulins@msn.com>
2018-12-10 13:55:27 -0200

    Update lolhack.sh
    Creates system backups and enables PCSX menu.

--- lolhack/lolhack.sh
+++ lolhack/lolhack.sh
@@ -1,25 +1,59 @@
-#link the usb drive dirs into the /gaadata
-cd /media/games
+#kill running processes
+killall -s KILL sonylogo sonyapp ui_menu
+
+
+#create a full backup image of the system (it can take up to 10 minutes)
+#/bin/dd if=/dev/mmcblk0 of=/media/ps_classic.img bs=4M
+
+
+#full restore from image
+
+
+#backup original system files and games (it will not overwrite files after the first backup)
+cp -fnR /data/* /media/data
+cp -fnR /gaadata/* /media/gaadata
+
+
+#restore original system files and games
+#cp -fR /media/data/* /data
+#cp -fR /media/gaadata/* /gaadata
+
+
+#backup original games db (it will not overwrite files after the first backup)
+cp -nR /gaadata/databases/regional.db /media/gaadata/databases/regional.db 
+
+
+#restore original games db
+#cp -f /media/gaadata/databases/regional.db /gaadata/databases/regional.db
+
+
+#link thumb drive dirs into the /gaadata (add to original games; game folder needs to start with 21)
+cd /media/lolhack/games
 for D in *; do
-    if [ -d "${D}" ]; then
-        ln -s /media/games/${D} /gaadata/${D}
-    fi
+	if [ -d "${D}" ]; then
+		ln -s /media/lolhack/games/${D} /gaadata/${D}
+	fi
 done
 
-#backup the original db (will not overwrite the file after the first backup)
-cp -n /gaadata/databases/regional.db /media/games/backup.db
+#replace games db (need a interface to create regional.db)
+cp /media/lolhack/databases/regional.db /gaadata/databases/regional.db
 
-#install our custom db
-cp /media/games/custom.db /gaadata/databases/regional.db
+
+#overmount the original games (replace original games; it will not rewrite original games; game folder needs to start with 1; what happens if I start to name the folders with 21? need an interface like hackchi to add games and create regional.db)
+#find * -maxdepth 0 -type d -exec mount -o bind /media/lolhack/games/{} /gaadata/{} \;
+
+
+#enable PCSX menu by pressing [SELECT] + [TRIANGLE] on the controller
+export PCSX_ESC_KEY=2
+
+
+#place cursor back in the first game
+sed -i "s/iUiUserSettingLastSelectGameCursorPos.*/iUiUserSettingLastSelectGameCursorPos=0/" /data/AppData/sony/ui/user.pre
+
+
+#restart ui
+/usr/sony/bin/ui_menu --power-off-enable
+
 
 #sync usb drive
 sync
-
-# kill the ui process
-killall ui_menu
-
-# restart ui
-/usr/sony/bin/ui_menu
-
-# sleep forever so the usb is never unmounted
-while :; do sleep 10; done
